e7273caac8c1a22d730453bc3154f0b405194cb1
Muhammad Ali <m13ali1000@gmail.com>
2026-02-26 16:34:54 +0500

    CORS error resolved

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 connectDB()
 app.use(helmet());
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL, 
-  credentials: true              
-}));
+app.use(cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
