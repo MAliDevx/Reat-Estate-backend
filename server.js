f037d521e81c79c1453a7a4551e70607e34e9b5e
Muhammad Ali <m13ali1000@gmail.com>
2026-04-16 11:34:05 +0500

    remove origin strictness

--- server.js
+++ server.js
@@ -9,25 +9,10 @@ const app = express();
 
 connectDB();
 
-// Build the list of allowed origins from env
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  'http://localhost:3000',
-  'http://localhost:5173',
-  'http://127.0.0.1:3000',
-  'http://127.0.0.1:5173',
-].filter(Boolean);
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (curl, Postman, mobile apps)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error(`CORS: origin ${origin} not allowed`));
-  },
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-token'],
-  credentials: true,
   optionsSuccessStatus: 200,
 };
 
